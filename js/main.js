4ce30a902049f53dff92d289ecf5446dda386d2f
Edgar Herrera <edgarh09lfc@gmail.com>
2022-06-06 11:48:09 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let total = 0
+
+// document.querySelector('#zero').addEventListener('click', makeZero)
+// document.querySelector('#one').addEventListener('click', jumanji)
+// document.querySelector('#two').addEventListener('click', addTwo)
+// document.querySelector('#three').addEventListener('click', sub2)
+
+
