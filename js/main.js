257fe77109b227f16114051f52b18278ecbf2ed5
Edgar Herrera <edgarh09lfc@gmail.com>
2022-06-19 00:55:28 -0700

    I added the append number function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
-let total = 0
-
-// document.querySelector('#zero').addEventListener('click', makeZero)
-// document.querySelector('#one').addEventListener('click', jumanji)
-// document.querySelector('#two').addEventListener('click', addTwo)
-// document.querySelector('#three').addEventListener('click', sub2)
 
 
-let doc = document.querySelector('.calc-buttons')
+class Calculator  {
+    constructor(currentOperandTextElement) {
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.operation = undefined
+    }
+    
+    //appendNumber section is done!
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) {
+            return;
+        }
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    pickOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+
+}
+
+const numbers = document.querySelectorAll('[data-number]')
+const operations = document.querySelectorAll('[data-operation]')
+const equals = document.querySelectorAll('[data-equals]')
+const allClear = document.querySelector('[data-all-clear]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+                                                
+
+const calculator = new Calculator(currentOperandTextElement)
+
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
